0ead2ea2e6789bafb9b165bd2b6fa2ca567f2599
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-16 10:39:50 -0700

    Add prop to cancel reservation

--- src/Container/Container.js
+++ src/Container/Container.js
@@ -2,26 +2,27 @@ import React from 'react';
 import Card from '../Card/Card';
 import '../index.css'
 
-const Container = ({ reservations }) => {
-  const reservationCards = reservations.map(reservation => {
-    return(
+const Container = ({ reservations, cancelReservation }) => {
+  const reservationCards = reservations.map((reservation) => {
+    return (
       <div key={reservation.id}>
         <Card
+          id={reservation.id}
           name={reservation.name}
           date={reservation.date}
           time={reservation.time}
           number={reservation.number}
-          />
+          cancelReservation={cancelReservation}
+        />
       </div>
-    )
-  })
-  return(
-    <div className='body'>
-      <h2>Contianer</h2>
+    );
+  });
+  return (
+    <div className="body">
       {reservationCards}
     </div>
-  )
-}
+  );
+};
 
 
 export default Container;
