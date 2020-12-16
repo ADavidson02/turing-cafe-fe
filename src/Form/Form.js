048bad52fc2c73016739b84682f384a0edabffb3
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-16 10:28:41 -0700

    Add new reservation functionality

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -13,39 +13,61 @@ class Form extends Component {
     }
   }
 
+  submitReservation = event => {
+    event.preventDefault();
+    const newReservation = {
+      id: Date.now(),
+      ...this.state
+    }
+    this.props.addReservation(newReservation)
+    this.clearForm()
+  }
+
+  clearForm = () => {
+    this.setState({name: '', date: '', time: '', time:'', number: ''})
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value})
+  }
+
   render() {
-    return(
+    return (
       <form>
-        <input 
-        type='text'
-        name='name'
-        placeholder='Name'
-        value={this.state.name}
-        />
-
-        <input 
-        type='text'
-        name='date'
-        placeholder='Date (mm/dd)'
-        value={this.state.date}
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={(event) => this.handleChange(event)}
         />
 
         <input
-        type='text'
-        name='time'
-        placeholder='Time'
-        value={this.state.time}
+          type="text"
+          name="date"
+          placeholder="Date (mm/dd)"
+          value={this.state.date}
+          onChange={(event) => this.handleChange(event)}
         />
 
         <input
-        type='text'
-        name='number'
-        placeholder='Number of guests'
-        value={this.state.number}
+          type="text"
+          name="time"
+          placeholder="Time"
+          value={this.state.time}
+          onChange={(event) => this.handleChange(event)}
         />
-        <button className='submit-button'>Make Reservation </button>
+
+        <input
+          type="text"
+          name="number"
+          placeholder="Number of guests"
+          value={this.state.number}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <button className="submit-button" onClick={event => this.submitReservation(event)}>Make Reservation </button>
       </form>
-    )
+    );
   }
 }
 
