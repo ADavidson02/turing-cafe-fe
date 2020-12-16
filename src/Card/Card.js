0d71f01824822f089c5f56edf391adaed575c7b1
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-16 10:39:24 -0700

    Add functionality to cancel reservation

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import './Card.css'
+import './Card.css';
+import Container from '../Container/Container';
 
-const Card = ({ id, name, date, time, number }) => {
-  return(
-    <div className='card' key={id}>
+const Card = ({ id, name, date, time, number, cancelReservation }) => {
+  return (
+    <div className="card" key={id} id={id}>
       <h3>{name}</h3>
       <p>{date}</p>
       <p>{time} pm</p>
       <p>Number of guests: {number}</p>
-      <button>Cancel</button>
+      <button onClick={() => cancelReservation(id)}>Cancel</button>
     </div>
-  )
-}
+  );
+};
 
 
 export default Card; 
