b8b22ab7c94920cb592e0e98e55c1c8ca24afdb0
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-16 09:58:19 -0700

    Fix container file structure to be modular

---
+++ src/Container/Container.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Card from '../Card/Card';
+import '../index.css'
+
+const Container = ({ reservations }) => {
+  const reservationCards = reservations.map(reservation => {
+    return(
+      <div key={reservation.id}>
+        <Card
+          name={reservation.name}
+          date={reservation.date}
+          time={reservation.time}
+          number={reservation.number}
+          />
+      </div>
+    )
+  })
+  return(
+    <div className='body'>
+      <h2>Contianer</h2>
+      {reservationCards}
+    </div>
+  )
+}
+
+
+export default Container;
