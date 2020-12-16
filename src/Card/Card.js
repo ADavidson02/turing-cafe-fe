4245f2c8f1fc307cdc4740cd75e818a1a518439e
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-16 09:57:08 -0700

    Add modular file structure

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Card.css'
 
 const Card = ({ id, name, date, time, number }) => {
   return(
-    <div key={id}>
+    <div className='card' key={id}>
       <h3>{name}</h3>
       <p>{date}</p>
-      <p>{time}</p>
-      <p>{number}</p>
+      <p>{time} pm</p>
+      <p>Number of guests: {number}</p>
       <button>Cancel</button>
     </div>
   )
