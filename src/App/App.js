7298170c2e06b56d98e5f632b85e61a4a1b9c007
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-16 10:39:00 -0700

    Add functionality to add and remove reservations

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Container from '../Container/Container';
 import { getAllReservation } from '../apiCall';
+import Form from '../Form/Form';
 
 class App extends Component {
   constructor() {
@@ -17,20 +18,31 @@ class App extends Component {
     console.log(allReservations);
   }
 
+  addReservation = (newReservation) => {
+    this.setState({reservations: [...this.state.reservations, newReservation]})
+  }
+
+  cancelReservation = (id) => {
+    const filteredReservations = this.state.reservations.filter(reservation => reservation.id !== id )
+    this.setState({ reservations: filteredReservations })
+  }
+
 
   render() {
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <div className="resy-form">
+          <Form addReservation={this.addReservation} />
         </div>
-        <div className='resy-container'>
-          <Container reservations={this.state.reservations}/>
-          
+        <div className="resy-container">
+          <Container
+            reservations={this.state.reservations}
+            cancelReservation={this.cancelReservation}
+          />
         </div>
       </div>
-    )
+    );
   }
 }
 
