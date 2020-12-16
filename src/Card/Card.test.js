3f1dd03cf175f4937b4da4108ae3219d5b36ab69
Amanda Davidson <AmandaADavidson@hotmail.com>
2020-12-16 11:20:43 -0700

    Add test to check called with correct id

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -4,24 +4,44 @@ import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import Card from "./Card";
 
-
-describe('Card', () => {
-  it('should render correctly', () => {
+describe("Card", () => {
+  it("should render correctly", () => {
     render(
-    <Card 
-      id={3}
-      name='Sadie'
-      date='01/24'
-      time='4:50'
-      number={4}
-      cancelReservation={jest.fn()}
-    />)
-  
-    expect(screen.getByText('Sadie')).toBeInTheDocument();
-    expect(screen.getByText('01/24')).toBeInTheDocument();
-    expect(screen.getByText('4:50 pm')).toBeInTheDocument();
-    expect(screen.getByText('Number of guests: 4')).toBeInTheDocument();
-    expect(screen.getByText('Cancel')).toBeInTheDocument();
-  })
+      <Card
+        id={3}
+        name="Sadie"
+        date="01/24"
+        time="4:50"
+        number={4}
+        cancelReservation={jest.fn()}
+      />
+    );
 
-})
+    expect(screen.getByText("Sadie")).toBeInTheDocument();
+    expect(screen.getByText("01/24")).toBeInTheDocument();
+    expect(screen.getByText("4:50 pm")).toBeInTheDocument();
+    expect(screen.getByText("Number of guests: 4")).toBeInTheDocument();
+    expect(screen.getByText("Cancel")).toBeInTheDocument();
+  }),
+  
+  it("should call cancel reservation with the correct id", () => {
+    const mockCancelReservation = jest.fn();
+
+    render(
+      <Card
+        id={3}
+        name="Zoe"
+        date="02/22"
+        time="200"
+        number={2}
+        cancelReservation={mockCancelReservation}
+      />
+    );
+
+    const cancelButton = screen.getByText("Cancel");
+
+    userEvent.click(cancelButton);
+
+    expect(mockCancelReservation).toHaveBeenCalledWith(3);
+  });
+});
